aa4805bab3931d97639d66c218700abec539ca43
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2023-08-07 13:20:17 +0300

    add pagination for the students and payments

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -12,10 +12,15 @@ exports.createStudent = async (req, res) => {
   }
 };
 
-// Get all students
+// Get all students with pagination
 exports.getStudents = async (req, res) => {
   try {
-    const students = await Student.find();
+    const { page = 1, perPage = 10 } = req.query;
+    const options = {
+      skip: (page - 1) * perPage,
+      limit: parseInt(perPage),
+    };
+    const students = await Student.find({}, null, options);
     res.json(students);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -42,10 +47,14 @@ exports.updateStudent = async (req, res) => {
     const { studentId } = req.params;
     const studentData = req.body;
 
-    const updatedStudent = await Student.findByIdAndUpdate(studentId, studentData, {
-      new: true, // Return the updated document
-      runValidators: true, // Run model validations on the update
-    });
+    const updatedStudent = await Student.findByIdAndUpdate(
+      studentId,
+      studentData,
+      {
+        new: true, // Return the updated document
+        runValidators: true, // Run model validations on the update
+      }
+    );
 
     if (!updatedStudent) {
       return res.status(404).json({ error: "Student not found" });
@@ -90,7 +99,9 @@ exports.createPayment = async (req, res) => {
 
     // Check if the payment amount exceeds the student's fee
     if (amount > student.fee) {
-      return res.status(400).json({ error: "Payment amount exceeds student's fee" });
+      return res
+        .status(400)
+        .json({ error: "Payment amount exceeds student's fee" });
     }
 
     // Create a payment
@@ -107,101 +118,100 @@ exports.createPayment = async (req, res) => {
   }
 };
 
-// Get all payments for a student
+// Get all payments for a student with pagination
 exports.getPaymentsForStudent = async (req, res) => {
   try {
     const { studentId } = req.params;
-    const payments = await Payment.find({ student: studentId });
+    const { page = 1, perPage = 10 } = req.query;
+    const options = {
+      skip: (page - 1) * perPage,
+      limit: parseInt(perPage),
+    };
+    const payments = await Payment.find({ student: studentId }, null, options);
     res.json(payments);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-// update payment for a student
 // Update a payment for a student
 exports.updatePayment = async (req, res) => {
-    try {
-      const { studentId, paymentId } = req.params;
-      const { amount, comments } = req.body;
-  
-      // Find the student by ID
-      const student = await Student.findById(studentId);
-      if (!student) {
-        return res.status(404).json({ error: "Student not found" });
-      }
-  
-      // Find the payment by ID
-      const payment = await Payment.findById(paymentId);
-      if (!payment) {
-        return res.status(404).json({ error: "Payment not found" });
-      }
-  
-      // Calculate the difference between the new payment amount and the original payment amount
-      const paymentDiff = amount - payment.amount;
-  
-      // Update the payment
-      payment.amount = amount;
-      payment.comments = comments;
-      await payment.save();
-  
-      // Update student's fee by subtracting the paymentDiff
-      student.fee -= paymentDiff;
-      await student.save();
-  
-      res.json({ message: "Payment updated successfully" });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  try {
+    const { studentId, paymentId } = req.params;
+    const { amount, comments } = req.body;
+
+    // Find the student by ID
+    const student = await Student.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
     }
-  };
-  
-  
-  
-  
-  
-  // Get a single payment for a student
-  exports.getPaymentById = async (req, res) => {
-    try {
-      const { studentId, paymentId } = req.params;
-  
-      // Find the payment by ID
-      const payment = await Payment.findById(paymentId);
-      if (!payment) {
-        return res.status(404).json({ error: "Payment not found" });
-      }
-  
-      res.json(payment);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+    // Find the payment by ID
+    const payment = await Payment.findById(paymentId);
+    if (!payment) {
+      return res.status(404).json({ error: "Payment not found" });
     }
-  };
-  
-  // Delete a payment for a student
-  exports.deletePayment = async (req, res) => {
-    try {
-      const { studentId, paymentId } = req.params;
-  
-      // Find the student by ID
-      const student = await Student.findById(studentId);
-      if (!student) {
-        return res.status(404).json({ error: "Student not found" });
-      }
-  
-      // Find the payment by ID
-      const payment = await Payment.findById(paymentId);
-      if (!payment) {
-        return res.status(404).json({ error: "Payment not found" });
-      }
-  
-      // Update student's fee
-      student.fee += payment.amount;
-      await student.save();
-  
-      // Delete the payment
-      await payment.remove();
-  
-      res.json({ message: "Payment deleted successfully" });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+    // Calculate the difference between the new payment amount and the original payment amount
+    const paymentDiff = amount - payment.amount;
+
+    // Update the payment
+    payment.amount = amount;
+    payment.comments = comments;
+    await payment.save();
+
+    // Update student's fee by subtracting the paymentDiff
+    student.fee -= paymentDiff;
+    await student.save();
+
+    res.json({ message: "Payment updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get a single payment for a student
+exports.getPaymentById = async (req, res) => {
+  try {
+    const { studentId, paymentId } = req.params;
+
+    // Find the payment by ID
+    const payment = await Payment.findById(paymentId);
+    if (!payment) {
+      return res.status(404).json({ error: "Payment not found" });
     }
-  };
-  
+
+    res.json(payment);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete a payment for a student
+exports.deletePayment = async (req, res) => {
+  try {
+    const { studentId, paymentId } = req.params;
+
+    // Find the student by ID
+    const student = await Student.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+
+    // Find the payment by ID
+    const payment = await Payment.findById(paymentId);
+    if (!payment) {
+      return res.status(404).json({ error: "Payment not found" });
+    }
+
+    // Update student's fee
+    student.fee += payment.amount;
+    await student.save();
+
+    // Delete the payment
+    await payment.remove();
+
+    res.json({ message: "Payment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
